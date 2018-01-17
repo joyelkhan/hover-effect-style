97f05bfa6aefe3878140c55a9b2734b2c1d7835b
joyelkhan <joyelkhan2@gmail.com>
2018-01-17 14:20:07 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*=========================================
+
+Template 
+Author: MD Abunaser Khan
+Version: 1.0
+
+=========================================*/
+
+
+(function ($) {
+	"use strict";
+    
+   
+
+}(jQuery));	
+
